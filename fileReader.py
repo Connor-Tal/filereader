ec5bc434a6a06e2f765b6345eb4bb8e9d5eb5657
Connor <connortalbott21@gmail.com>
2022-09-06 18:16:46 -0700

    Major fixes and version history

--- fileReader.py
+++ fileReader.py
@@ -1,18 +1,26 @@
 import time
 
 file = open(r"file name","r+")
-line = 0
-keyword = input("What word are you searching for?")
 content = file.readlines()
+line = 0
+wordcount = 0
+
+keyword = input("What word are you searching for?")
 while True:
     try:
-        print(content[line])
         if content[line].__contains__(keyword):
-            print("keyword found!", keyword)
-            file.close()
-            break
-        time.sleep(0.25)
-        line += 1
+            wordcount += 1
+            print("The word you are looking for is on line:", line + 1)
+            print ("This is the line:", content[line])
+            time.sleep(1)
+            line += 1
+        else:    
+            line += 1
     except:
-        print("you ran out of lines and no word was found")
-        exit()
+        if wordcount == 0:
+            print("The word you were looking for is not in the file")
+            exit()
+        else:
+            print("There were a total of", wordcount,"words in the document")
+            file.close()
+            exit()
