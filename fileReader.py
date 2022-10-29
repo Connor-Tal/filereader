d50eb8edcf1fed2d036ab288e744734ac5c7cc35
Connor <connortalbott21@gmail.com>
2022-10-29 12:54:20 -0700

    Known issue: program can create blank lines

--- fileReader.py
+++ fileReader.py
@@ -1,26 +1,41 @@
 import time
 
-file = open(r"file name","r+")
-content = file.readlines()
 line = 0
 wordcount = 0
+file = open('text.txt', 'r')
+data = file.read()
+original = file.read()
+keep = input('Do you want to keep to keep the original format (y)? Or have the program make a new line after each space (n)? (y/n)')
+if keep == 'y':
+    print('original data kept')
+if keep == 'n':
+    data = data.replace('.', '.\n')
+    file.close()
+    file = open('text.txt', 'w')
+    file.write(' ')
+    file.write(data)
+    file.close()
 
 keyword = input("What word are you searching for?")
+
 while True:
     try:
-        if content[line].__contains__(keyword):
-            wordcount += 1
-            print("The word you are looking for is on line:", line + 1)
-            print ("This is the line:", content[line])
-            time.sleep(1)
-            line += 1
-        else:    
-            line += 1
+        with open('text.txt',"r") as file:
+            content = file.readlines()
+            if content[line].__contains__(keyword):
+                wordcount += 1
+                print(f"The word you are looking for is on line: {line + 1}")
+                print (f"This is the line: {content[line]}")
+                time.sleep(1)
+                line += 1
+            else:    
+                line += 1
     except:
         if wordcount == 0:
             print("The word you were looking for is not in the file")
+            file.close()
             exit()
         else:
-            print("There were a total of", wordcount,"words in the document")
+            print(f"There were a total of the {wordcount} of the words you were looking for in the document")
             file.close()
             exit()
